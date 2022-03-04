bf910dfa3a4456d86d80cbaca479491746fa43d1
Kernel Soe <kernelsoe@gmail.com>
2022-03-04 10:51:39 +0900

    done

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,5 +1,12 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
-  end
+  def compare(secret, guess \\ :no_guess)
+
+  def compare(secret, guess) when secret === guess, do: "Correct"
+
+  def compare(_, guess) when not is_integer(guess), do: "Make a guess"
+
+  def compare(secret, guess) when abs(secret - guess) == 1, do: "So close"
+
+  def compare(secret, guess) when secret > guess, do: "Too low"
+  def compare(secret, guess) when secret < guess, do: "Too high"
 end
